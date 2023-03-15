7188cefaedadaba7925dfada13a8f19382fba027
DodoBirb <roanvanelven@gmail.com>
2023-03-16 08:23:19 +1100

    AM2RClient can now send and receive data

--- AM2RClient.py
+++ AM2RClient.py
@@ -24,6 +24,11 @@ class AM2RCommandProcessor(ClientCommandProcessor):
     def __init__(self, ctx: CommonContext):
         super().__init__(ctx)
 
+    def _cmd_am2r(self):
+        """Check AM2R Connection State"""
+        if isinstance(self.ctx, AM2RContext):
+            logger.info(f"Connection Status: {self.ctx.am2r_status}")
+
 class AM2RContext(CommonContext):
     command_processor = AM2RCommandProcessor
     game = 'AM2R'
@@ -59,21 +64,65 @@ class AM2RContext(CommonContext):
         self.ui_task = asyncio.create_task(self.ui.async_run(), name="UI")
 
 async def am2r_sync_task(ctx: AM2RContext):
-    logger.info("Starting AM2R connector")
+    logger.info("Starting AM2R connector, use /am2r for status information.")
     while not ctx.exit_event.is_set():
         error_status = None
-        try:
-            logger.debug("Attempting to connect to AM2R")
-            ctx.am2r_streams = await asyncio.wait_for(asyncio.open_connection("localhost", 64197), timeout=10)
-            ctx.am2r_status = CONNECTION_TENTATIVE_STATUS
-        except TimeoutError:
-            logger.debug("Connection Timed Out, Trying Again")
-            ctx.am2r_status = CONNECTION_TIMING_OUT_STATUS
-            continue
-        except ConnectionRefusedError:
-            logger.debug("Connection Refused, Trying Again")
-            ctx.am2r_status = CONNECTION_REFUSED_STATUS
-            continue
+        if ctx.am2r_streams:
+            (reader, writer) = ctx.am2r_streams
+            msg = "HelloWorld".encode()
+            writer.write(msg)
+            writer.write(b'\n')
+            try:
+                print("Hi")
+                await asyncio.wait_for(writer.drain(), timeout=1.5)
+                print("Ho")
+                try:
+                    data = await asyncio.wait_for(reader.readline(), timeout=5)
+                    data_decoded = data.decode()
+                    logger.info(data_decoded)
+                except asyncio.TimeoutError:
+                    logger.debug("Read Timed Out, Reconnecting")
+                    error_status = CONNECTION_TIMING_OUT_STATUS
+                    writer.close()
+                    ctx.am2r_streams = None
+                except ConnectionResetError as e:
+                    logger.debug("Read failed due to Connection Lost, Reconnecting")
+                    error_status = CONNECTION_RESET_STATUS
+                    writer.close()
+                    ctx.am2r_streams = None
+            except TimeoutError:
+                logger.debug("Connection Timed Out, Reconnecting")
+                error_status = CONNECTION_TIMING_OUT_STATUS
+                writer.close()
+                ctx.am2r_streams = None
+            except ConnectionResetError:
+                logger.debug("Connection Lost, Reconnecting")
+                error_status = CONNECTION_RESET_STATUS
+                writer.close()
+                ctx.am2r_streams = None
+
+            if ctx.am2r_status == CONNECTION_TENTATIVE_STATUS:
+                if not error_status:
+                    logger.info("Successfully Connected to AM2R")
+                    ctx.am2r_status = CONNECTION_CONNECTED_STATUS
+                else:
+                    ctx.am2r_status = f"Was tentatively connected but error occured: {error_status}"
+            elif error_status:
+                ctx.am2r_status = error_status
+                logger.info("Lost connection to AM2R and attempting to reconnect. Use /am2r for status updates")
+        else:
+            try:
+                logger.debug("Attempting to connect to AM2R")
+                ctx.am2r_streams = await asyncio.wait_for(asyncio.open_connection("localhost", 64197), timeout=10)
+                ctx.am2r_status = CONNECTION_TENTATIVE_STATUS
+            except TimeoutError:
+                logger.debug("Connection Timed Out, Trying Again")
+                ctx.am2r_status = CONNECTION_TIMING_OUT_STATUS
+                continue
+            except ConnectionRefusedError:
+                logger.debug("Connection Refused, Trying Again")
+                ctx.am2r_status = CONNECTION_REFUSED_STATUS
+                continue
 
 
 if __name__ == '__main__':
